ed133baf21b737fd2217693f30e81d1a8248c96b
Álvaro Marques <alvaromarques18@gmail.com>
2025-02-26 21:28:48 -0300

    feat: add theme context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "@radix-ui/themes/styles.css";
+import {ThemeContextProvider} from "./contexts/ThemeContext.tsx";
+import App from "./App.tsx";
+import "./styles/global.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </StrictMode>,
-)
+);
