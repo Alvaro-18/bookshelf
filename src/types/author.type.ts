68de5c0739bf6d3b68744e91fcda54ea32a5b194
Álvaro Marques <alvaromarques18@gmail.com>
2025-02-28 19:51:19 -0300

    refactor: remove categories from author type

--- src/types/author.type.ts
+++ src/types/author.type.ts
@@ -1,11 +1,9 @@
-import {Book} from "./book.type";
-
 export type Author = {
   id: string;
   fullName: string;
   biography?: string;
   dateOfBirth?: string;
   nationality?: string;
-  literaryGenres: string[];
-  books?: Book[];
+  category?: string;
+  menu: boolean
 };
